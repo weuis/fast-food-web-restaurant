eb56a1c25f1e155fa5d2072e125d8ee2556a1b04
Алексей Розеган <rozeganalexey@gmail.com>
2025-03-09 15:54:26 +0100

    Customizing admin panel

--- restaurant_app/admin.py
+++ restaurant_app/admin.py
@@ -2,9 +2,35 @@ from django.contrib import admin
 from .models import Position, PositionList, Chef, AboutUs, Feedback, BookTable
 
 
-admin.site.register(Position)
-admin.site.register(PositionList)
-admin.site.register(Chef)
-admin.site.register(AboutUs)
-admin.site.register(Feedback)
-admin.site.register(BookTable)
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("id", "position_name")
+    search_fields = ("position_name",)
+
+@admin.register(PositionList)
+class PositionListAdmin(admin.ModelAdmin):
+    list_display = ("id", "position_name", "price", "category", "chef")
+    search_fields = ("position_name", "category__position_name", "chef__username")
+    list_filter = ("category", "chef")
+
+@admin.register(Chef)
+class ChefAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "is_chef", "email")
+    search_fields = ("username", "email")
+    list_filter = ("is_chef",)
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ("id", "description",)
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_name", "rating")
+    search_fields = ("user_name",)
+    list_filter = ("rating",)
+
+@admin.register(BookTable)
+class BookTableAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "phone_number", "email", "total_person", "booking_date")
+    search_fields = ("name", "email", "phone_number")
+    list_filter = ("booking_date", "total_person")
