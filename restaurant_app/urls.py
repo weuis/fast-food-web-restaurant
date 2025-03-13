474d4b9475a20e7e38658c39fd50f469928439c9
Алексей Розеган <rozeganalexey@gmail.com>
2025-03-13 20:03:01 +0100

    First view function and simple home.html template

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from restaurant_app.views import home
-
 urlpatterns = [
-    path("", home, name="home"),
+    path("", home, name="index"),
 ]
+
+app_name= "restaurant_app"
--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 
 from django.http import HttpRequest, HttpResponse
 
+from .models import Position, PositionList, Feedback
+
 def home(request: HttpRequest) -> HttpResponse:
-    pass
+    positions = Position.objects.all()
+    position_list = PositionList.objects.all()
+    review = Feedback.objects.all()
+
+    context = {
+        'positions': positions,
+        'position_list': position_list,
+        'review': review
+    }
+    return render(
+        request,
+        'restaurant_app/home.html',
+        context=context
+    )
+
