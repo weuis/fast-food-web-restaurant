8f4a97ce331782db5f49aa6bbe59516ea1855648
Алексей Розеган <rozeganalexey@gmail.com>
2025-03-19 17:53:35 +0100

    Creating a first  menu.html template and menu view function

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from restaurant_app.views import home, about
+from restaurant_app.views import home, about, menu
 urlpatterns = [
     path("", home, name="home"),
     path("about/", about, name="about"),
+
+    path("menu/", menu, name="menu"),
+
+
 ]
 
 app_name= "restaurant_app"
