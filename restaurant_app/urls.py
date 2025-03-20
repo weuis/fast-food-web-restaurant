105fd18ce5a30a4e0690283c2be0634cad785a3e
Алексей Розеган <rozeganalexey@gmail.com>
2025-03-20 18:31:19 +0100

    Fix errora in book_table function based view and improve html file

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from restaurant_app.views import home_view, about_view, menu_view, book_table_view
+from restaurant_app.views import (
+    home_view,
+    about_view,
+    menu_view,
+    book_table_view,
+    feedback_view,
+)
 urlpatterns = [
     path("", home_view, name="home"),
     path("about/", about_view, name="about"),
@@ -8,6 +14,8 @@ urlpatterns = [
     path("menu/", menu_view, name="menu"),
 
     path("book-table/", book_table_view, name="book_table" ),
+
+    path("feedback/", feedback_view, name="feedback"),
 
 
 ]
