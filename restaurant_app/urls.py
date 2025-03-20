1761166c044689dfc9bf59dccca117866117d194
Алексей Розеган <rozeganalexey@gmail.com>
2025-03-20 17:57:29 +0100

    Creating book_table html file and book_table function based view

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from restaurant_app.views import home, about, menu
+from restaurant_app.views import home_view, about_view, menu_view, book_table_view
 urlpatterns = [
-    path("", home, name="home"),
-    path("about/", about, name="about"),
+    path("", home_view, name="home"),
+    path("about/", about_view, name="about"),
 
-    path("menu/", menu, name="menu"),
+    path("menu/", menu_view, name="menu"),
+
+    path("book-table/", book_table_view, name="book_table" ),
 
 
 ]
